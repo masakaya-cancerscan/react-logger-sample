1063db9437ea60c14cbf3834e49d42f3f148e257
Masashi Kayahara <masashi.kayahara@cancerscan.jp>
2022-10-19 17:06:15 +0900

    Add loglevel

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import './App.css';
+import log from 'loglevel'
+
 
 function App() {
+    log.setLevel("info")
+    log.debug("123456")
+    log.info("123456")
+    log.warn("123456")
+    log.error("123456")
+
   return (
     <div>
       <header className="App-header">
